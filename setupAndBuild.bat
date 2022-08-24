755362a485f2f7477c5612ae237ada8bab946894
M2ke4U <79621885+wangxyper@users.noreply.github.com>
2022-08-24 18:15:19 +0800

    Delete setupAndBuild.bat

--- setupAndBuild.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@echo "Downloading libraries..."
-mkdir Libs
-curl https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/standalone/target/Geyser.jar
-move Geyser.jar Lib
-@echo "Building..."
-mvn clean package
