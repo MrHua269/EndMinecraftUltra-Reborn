1ba1e1c94823857e2f12d80b57f7a3632b34f38e
M2ke4U <79621885+wangxyper@users.noreply.github.com>
2022-08-24 18:15:41 +0800

    Update setupAndBuild.sh

--- setupAndBuild.sh
+++ setupAndBuild.sh
@@ -1,3 +1,1 @@
-wget https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/standalone/target/Geyser.jar
-mkdir Libs && mv Geyser.jar Libs/
-mvn clean package
+wget https://ci.opencollab.dev//job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/standalone/target/Geyser.jar && mkdir Libs && mv Geyser.jar Libs/ && mvn clean package
